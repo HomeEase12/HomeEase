543defbef2ed4379a7749ea99be669451a080eac
root <dipw25@gmail.com>
2023-08-26 16:40:59 +0530

    banner change

--- React/src/components/Shared/Banner.js
+++ React/src/components/Shared/Banner.js
@@ -7,7 +7,7 @@ import headerImg from "../../Assets/image/WishItHome.jpg"
 export const Banner=()=>{
     const [loopNum,setLoopNum]=useState(0);
     const [isDeleting,setIsDeleting]=useState(false);
-    const toRotate=["WishIt"]
+    const toRotate=["HomeEase"]
     const[text,setText]=useState('');
     const [delta,setDelta] =useState(300 - Math.random() * 100);
     const period=2000;
